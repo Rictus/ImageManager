52c26defbbbb1e49245ae640db1a8dd0a5930057
Rictus <contact.dylanleroux@gmail.com>
2016-09-20 19:41:23 +0200

    Some propositions

--- ImageManager.cpp
+++ ImageManager.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "ImageManager.h"
+
+/*
+ * TODO
+ * Lire une image et la sauvegarder sans traitement special
+ */
+
+void ImageManager::loadPGM() {
+    // TODO
+}
+
+void ImageManager::loadPPM() {
+
+}
+
+void ImageManager::savePPM() {
+
+}
+
+void ImageManager::savePGM() {
+
+}
--- ImageManager.h
+++ ImageManager.h
@@ -4,9 +4,19 @@
 
 #ifndef IMAGEMANAGER_IMAGEMANAGER_H
 #define IMAGEMANAGER_IMAGEMANAGER_H
+#include <iostream>
+#include <bitset>
 
+using namespace std;
+
+/** We could go further and define the class Byte
+ *  and overloading some operators (+, -, =, [], ...)
+ */
+typedef bitset<8> byte; // From 00000000 (0) to 11111111 (254)
 
 class ImageManager {
+private:
+    byte**_image; // Let's find something better than **
 public:
     void loadPPM();
 
@@ -15,6 +25,8 @@ public:
     void savePPM();
 
     void savePGM();
+
+    void applySobelMask();
 };
 
 
