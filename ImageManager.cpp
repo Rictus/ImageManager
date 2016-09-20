1e5c6a0adb8b4218232c18045ef7f5f83545eb40
Rictus <contact.dylanleroux@gmail.com>
2016-09-20 14:30:18 +0200

    First files

---
+++ ImageManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dylan on 20/09/2016.
+//
+
+#include "ImageManager.h"
---
+++ ImageManager.h
@@ -0,0 +1,14 @@
+//
+// Created by Dylan on 20/09/2016.
+//
+
+#ifndef COMPTEUR_IMAGEMANAGER_H
+#define COMPTEUR_IMAGEMANAGER_H
+
+
+class ImageManager {
+
+};
+
+
+#endif //COMPTEUR_IMAGEMANAGER_H
